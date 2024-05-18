a377964be36c0182a3dbba2c6a99a782df4f87c2
Micah Woodring <micahkwoodring@gmail.com>
2024-05-17 22:59:48 -1000

    Replaced CSS with Tailwind

--- over-under/src/app/page.js
+++ over-under/src/app/page.js
@@ -8,23 +8,24 @@ export default function Home() {
 
 function homeScreen() {
   return (
-    <div className="container">
-      <div className="topNavBar">
-        <h1 className="topNavBarHeader">Over Under</h1>
-        <ul className="topNavBarButtons">
-          <li> <a href="https://duckduckgo.com/" target="_blank"> Home </a> </li>
-          <li> <a href="https://github.com/EliteMicah/Over-Under" target="_blank"> GitHub </a> </li>
+    <div className="m-0 box-border p-0">
+      <div className="flex fixed overflow-hidden top-0 h-75 w-full items-center justify-between bg-header-grey">
+        <h1 className="m-5 font-bold text-2xl font-times text-white">Over Under</h1>
+        <ul className="flex px-3 items-center text-white m-5">
+          <li className="mr-4"> <a href="https://duckduckgo.com/" target="_blank"> Home </a> </li>
+          <li className="mr-4"> <a href="https://github.com/EliteMicah/Over-Under" target="_blank"> GitHub </a> </li>
           <li> <a href="https://www.linkedin.com/in/micahwoodring/" target="_blank"> LinkedIn </a> </li>
         </ul>
       </div>
 
-      <div className="middleContent">
-        <div className="cardsContainer">
-            <div className="card a">
+      <div className="pt-75 h-[1500px] w-screen bg-gray-600 bg-gradient-to-b from-[#0e1419] to-[#292929]">
+        <div className="flex justify-center flex-wrap border-green-800 border-4 min-h-screen container mx-auto">
+            <div className="border-4 border-sky-800 rounded-2xl w-[400px] h-[400px] m-auto mx-6 text-white flex flex-col items-center justify-center">
               <p className="cardName">Create</p>
-              <p className="cardText">Create a game with your friends!</p>
+              <p className="cardText">Create a game to play with your friends!</p>
             </div>
-            <div className="card b">
+
+            <div className="border-4 border-sky-800 rounded-2xl w-[400px] h-[400px] m-auto mx-6 text-white flex flex-col items-center justify-center">
               <p className="cardName">Join</p>
               <p className="cardText">Join a game and begin playing!</p>
             </div>
--- over-under/tailwind.config.js
+++ over-under/tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'times': ['Times New Roman, Times, serif'],
+      },
+      colors: {
+        'header-grey':  '#222020',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-mytheme": "linear-gradient(to bottom, rgb(14, 20, 25), rgb(41, 41, 41))",
+      },
+      margin: {
+        '9p': '9%',
       },
     },
     spacing: {
@@ -20,6 +29,8 @@ module.exports = {
       '4': '24px',
       '5': '32px',
       '6': '48px',
+      '7': '60px',
+      '75': '75px',
     },
   },
   plugins: [],
