2d7aab53484a4a242107774144f372a74cae66bc
Micah Woodring <micahkwoodring@gmail.com>
2024-05-18 17:23:17 -1000

    More Tailwind CSS work, responsive cards

--- over-under/src/app/page.js
+++ over-under/src/app/page.js
@@ -11,16 +11,11 @@ function homeScreen() {
     <div className="m-0 box-border p-0">
       <div className="flex fixed overflow-hidden top-0 h-75 w-full items-center justify-between bg-header-grey">
         <h1 className="m-5 font-bold text-2xl font-times text-white">
-          Over Under
+          <a href="http://localhost:3000" target="_blank">
+            Over Under
+          </a>
         </h1>
         <ul className="flex px-3 items-center text-white m-5">
-          <li className="mr-4">
-            {" "}
-            <a href="https://duckduckgo.com/" target="_blank">
-              {" "}
-              Home{" "}
-            </a>{" "}
-          </li>
           <li className="mr-4">
             {" "}
             <a href="https://github.com/EliteMicah/Over-Under" target="_blank">
@@ -41,21 +36,32 @@ function homeScreen() {
         </ul>
       </div>
 
-      <div className="pt-75 h-[1500px] w-screen bg-gray-600 bg-gradient-to-b from-[#0e1419] to-[#292929]">
-        <div className="flex justify-center flex-wrap min-h-screen container m-auto border-4 border-sky-50">
+      <div className="h-screen pt-75 min-h-screen w-screen bg-gray-600 bg-gradient-to-b from-[#0e1419] to-[#292929]">
+        <div className="flex justify-center flex-wrap container mx-auto">
           <a
             href="#create-game"
-            className="no-underline block m-auto my-1 mx-16"
+            className="no-underline block mx-4 my-6 md:mx-6 md:my-8 lg:mx-10 lg:my-10"
           >
-            <div className="border-4 border-sky-800 rounded-2xl w-[400px] h-[400px] text-white flex flex-col items-center justify-center hover:bg-blue-900 hover:opacity-80">
+            <div
+              className="bg-gradient-to-b from-[#597993] to-[#292929] border-4 border-sky-800 rounded-2xl 
+              w-full sm:w-[150px] md:w-[250px] lg:w-[400px] h-full sm:h-[150px] md:h-[250px] lg:h-[400px] text-white flex flex-col items-center justify-center
+               hover:bg-blue-900 hover:opacity-80"
+            >
               <p className="text-5xl font-impact">Create</p>
               <p className="mt-5 text-center text-xl font-arial">
                 Create a game to play with your friends!
               </p>
             </div>
           </a>
-          <a href="#join-game" className="no-underline block m-auto mx-7">
-            <div className="border-4 border-sky-800 rounded-2xl w-[400px] h-[400px] text-white flex flex-col items-center justify-center hover:bg-blue-900 hover:opacity-80">
+          <a
+            href="#join-game"
+            className="no-underline block mx-4 my-6 md:mx-6 md:my-8 lg:mx-8 lg:my-10"
+          >
+            <div
+              className="bg-gradient-to-b from-[#597993] to-[#292929] border-4 border-sky-800 rounded-2xl 
+              w-full sm:w-[150px] md:w-[250px] lg:w-[400px] h-full sm:h-[150px] md:h-[250px] lg:h-[400px] text-white flex flex-col items-center justify-center
+               hover:bg-blue-900 hover:opacity-80"
+            >
               <p className="text-5xl font-impact">Join</p>
               <p className="mt-5 text-center text-xl font-arial">
                 Join a game and start playing!
