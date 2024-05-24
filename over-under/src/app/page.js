853824200a6581277774fbe0d267ee7c8f0f4412
Micah Woodring <micahkwoodring@gmail.com>
2024-05-23 23:03:06 -1000

    Failed to make proportionally scalable square cards. Reversed to old design.

--- over-under/src/app/page.js
+++ over-under/src/app/page.js
@@ -3,18 +3,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return homeScreen();
-}
-
-function homeScreen() {
   return (
     <div className="m-0 box-border p-0">
       <title>Over Under</title>
-      <div className="flex fixed overflow-hidden top-0 h-[75px] b-[75px] w-full items-center justify-between bg-header-grey">
+      <div className="flex fixed overflow-hidden top-0 h-[75px] w-full items-center justify-between bg-neutral-800 z-50">
         <h1 className="m-5 font-bold text-2xl font-times text-white">
-          <a href="http://localhost:3000" target="_blank">
+          <Link href="http://localhost:3000" target="_blank">
             Over Under
-          </a>
+          </Link>
         </h1>
         <ul className="flex px-3 items-center text-white m-5">
           <li className="mr-4">
@@ -41,36 +37,33 @@ function homeScreen() {
       </div>
 
       {/* Middle Content of the page */}
-      <div className="top-[75px] bg-gray-600 bg-gradient-to-b from-[#0e1419] to-[#292929]">
-        {/* CARD CONTAINER */}
-        <div className="flex flex-row items-center justify-center min-h-screen container mx-auto my-auto">
+      <div className="relative pt-[75px] w-lvw h-lvh bg-gray-600 bg-gradient-to-b from-[#0e1419] to-[#292929]">
+        {/* CARD SECTION */}
+        <div className="flex gap-4 items-center justify-center">
           {/* CREATE CARD */}
           <Link
             href="/Create-Game"
-            className="m-6 box-border square-card items-center justify-center bg-sky-100 rounded-xl py-2 
-              hover:bg-opacity-90 focus:scale-95 transition-all duration-75 ease-out shadow-md flex flex-wrap hover:scale-105"
+            className="m-6 box-border square-card items-center justify-center bg-sky-400 rounded-xl py-2 
+            hover:bg-opacity-90 focus:scale-95 transition-all duration-75 ease-out shadow-sm flex flex-col hover:scale-105
+            max-h-full max-w-full overflow-hidden text-center"
           >
-            <div className="overflow-hidden items-center justify-center flex-col">
-              <h5 className="text-6xl font-impact text-center">Create</h5>
-              <p className="text-slate-500 text-lg p-4 text-center">
-                Create a game to play with your friends!
-              </p>
-            </div>
+            <h2 className="text-6xl font-impact">Create</h2>
+            <p className="text-slate-300 text-lg p-4">
+              Create a game to play with your friends!
+            </p>
           </Link>
 
           {/* JOIN CARD */}
           <Link
             href="/Join-Game"
-            className="m-6 box-border square-card items-center justify-center bg-sky-100 rounded-xl py-2 
-              hover:bg-opacity-90 focus:scale-95 transition-all duration-75 ease-out shadow-sm flex flex-wrap hover:scale-105
-              max-h-full max-w-full"
+            className="m-6 box-border square-card items-center justify-center bg-sky-400 rounded-xl py-2 
+            hover:bg-opacity-90 focus:scale-95 transition-all duration-75 ease-out shadow-sm flex flex-col hover:scale-105
+            max-h-full max-w-full overflow-hidden text-center"
           >
-            <div className="overflow-hidden items-center flex-col">
-              <h5 className="text-6xl font-impact text-center">Join</h5>
-              <p className="text-slate-500 text-lg p-4 text-center">
-                Join a game to start playing!
-              </p>
-            </div>
+            <h2 className="text-6xl font-impact">Join</h2>
+            <p className="text-slate-300 text-lg p-4">
+              Join a game to start playing!
+            </p>
           </Link>
         </div>
       </div>
