721990ae2dc35318b04816273fafe035a1bcafd7
Kierstin <kierstinhavens@gmail.com>
2025-03-18 15:12:39 -0700

    added the link to the info page.

--- src/app/infoPage/page.tsx
+++ src/app/infoPage/page.tsx
@@ -1,16 +1,12 @@
-// cd SWE-Over-Under
-// npm run dev
-
-// For tailwind https://tailwindcss.com/
-
 "use client";
-
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import supabase from "@/config/supabaseClient";
 import "../../app/BackgroundAnimation.css";
 
-export default function HomePage() {
+function infoPage() {
   return (
-    <div className="h-screen w-screen bg-gray-200 flex-auto">
+    <div className="h-screen w-screen flex-auto bg-gray-200">
       <header className="mx-auto max-w-full h-20 items-center justify-between p-4 lg:px-8 flex bg-blue-200">
         <nav className="flex-row">
           <Link href={"/home"}>
@@ -34,17 +30,17 @@ export default function HomePage() {
             </Link>
           </button>
           <button className="hover:scale-105">
-            <Link href = {"/infoPage"}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="40px"
-                viewBox="0 -960 960 960"
-                width="40px"
-                fill="#000000"
-              >
-                <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-              </svg>
-            </Link>
+          <Link href={"/infoPage"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="40px"
+              viewBox="0 -960 960 960"
+              width="40px"
+              fill="#000000"
+            >
+              <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
+        </Link>
           </button>
           <button className="hover:scale-105">
             <Link href="/profile">
@@ -61,33 +57,8 @@ export default function HomePage() {
           </button>
         </nav>
       </header>
-
-      <div className="moving-background">
-        <div className="flex flex-row flex-wrap justify-center place-items-center">
-          <h2 className="font-Modak text-7xl mt-[80px] drop-shadow-md">
-            Welcome, &nbsp;
-          </h2>
-          <button className="hover:scale-105">
-            <Link href={"/profile"}>
-              <h2 className="font-Modak text-7xl flex-row mt-[80px] drop-shadow-md">
-                Username
-              </h2>
-            </Link>
-          </button>
-        </div>
-        <div className="h-auto w-[80%] mt-10 justify-evenly place-self-center flex flex-wrap">
-          <Link href={"/create"}>
-            <div className="h-96 w-96 min-w-48 min-h-12 flex flex-initial rounded-2xl bg-blue-300 drop-shadow-md hover:scale-105 duration-75 ease-out place-content-center place-items-center">
-              <h2 className="text-7xl font-Modak text-gray-50">Create</h2>
-            </div>
-          </Link>
-          <Link href={"/join"}>
-            <div className="h-96 w-96 min-w-48 min-h-12 flex flex-initial rounded-2xl bg-red-300 drop-shadow-md hover:scale-105 duration-75 ease-out place-content-center place-items-center">
-              <h2 className="text-7xl font-Modak text-gray-50">Join</h2>
-            </div>
-          </Link>
-        </div>
-      </div>
     </div>
   );
 }
+
+export default infoPage;
